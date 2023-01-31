edea605e4204e1555b092ade9c22f7a1dade0304
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-01-31 17:59:09 +0100

    add wikipedia languages

--- resources/views/livewire/geo-json/standalone-helper-view.blade.php
+++ resources/views/livewire/geo-json/standalone-helper-view.blade.php
@@ -238,9 +238,9 @@
                                         init() {
                                             const map = L.map($refs.map)
                                                 .setView([0, 0], 13);
-                                    
+
                                             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', { foo: 'bar', attribution: '&copy; <a href=\'https://www.openstreetmap.org/copyright\'>OpenStreetMap</a> contributors' }).addTo(map);
-                                    
+
                                             const geojsonFeature = {
                                                 'type': 'Feature',
                                                 'geometry': this.geojson
@@ -252,7 +252,7 @@
                                             L.geoJson(geojsonFeature, { style: { color: '#FFA500', fillColor: '#FFA500', fillOpacity: 0.3 } }).addTo(map);
                                             let simplifiedGeoJSON = L.geoJson(simplifiedGeojsonFeature, { style: { fillOpacity: 0.5 } }).addTo(map);
                                             map.fitBounds(simplifiedGeoJSON.getBounds(), { padding: [50, 50] });
-                                    
+
                                             $wire.on('geoJsonUpdated', () => {
                                                 map.eachLayer((layer) => {
                                                     layer.remove();
@@ -306,6 +306,12 @@
                             {{ $search }}</a>
                         <a target="_blank" class="text-amber-500 underline"
                             href="https://de.wikipedia.org/wiki/{{ urlencode($search) }}">Wikipedia DE:
+                            {{ $search }}</a>
+                        <a target="_blank" class="text-amber-500 underline"
+                            href="https://fr.wikipedia.org/wiki/{{ urlencode($search) }}">Wikipedia FR:
+                            {{ $search }}</a>
+                        <a target="_blank" class="text-amber-500 underline"
+                            href="https://es.wikipedia.org/wiki/{{ urlencode($search) }}">Wikipedia ES:
                             {{ $search }}</a>
                     </div>
                 </div>
