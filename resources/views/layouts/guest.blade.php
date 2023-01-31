54f16c10ffc4981de8303a2534b6bd991cca895f
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2023-01-31 09:57:47 -0700

    center div on mobile

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -35,7 +35,7 @@
 <body class="font-sans text-gray-900 antialiased">
     <x-notifications z-index="z-50" blur="md" align="center" />
     <div
-        class="flex min-h-screen flex-col items-center bg-gray-100 pt-2 dark:bg-gray-900 sm:justify-center sm:pt-6 sm:pt-0">
+        class="flex min-h-screen flex-col items-center justify-center bg-gray-100 pt-2 dark:bg-gray-900 sm:pt-6 sm:pt-0">
         <div
             class="my-2 w-full max-w-screen-2xl bg-white px-1 py-4 shadow-md dark:bg-gray-800 sm:my-6 sm:rounded-lg sm:px-6">
             {{ $slot }}
